d03cc3920b17eb37ab09748028190d8f01962233
Thalytaxxx <thalytaaaa08@gmail.com>
2025-11-04 13:03:55 -0300

    Create Main class with main method for game setup

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Tabuleiro tabuleiro = new Tabuleiro();
+        // Criar peças e adicionar ao tabuleiro
+        // Testar movimentação de peças
+    }
+}
