3766aeb7de3e573b556ab38f33fdae41997b394e
Thalytaxxx <thalytaaaa08@gmail.com>
2025-11-04 14:21:01 -0300

    Atualizar o Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro();
-        // Criar peças e adicionar ao tabuleiro
-        // Testar movimentação de peças
+        // Iniciar o jogo
+        tabuleiro.iniciarJogo();
     }
 }
